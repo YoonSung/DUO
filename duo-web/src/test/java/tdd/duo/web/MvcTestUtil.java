206c0052f01a69382b37d4f42455e7e2390a6aee
Yoonsung <lvev9925@naver.com>
2015-04-06 22:03:04 +0900

    Add Untrackted file

---
+++ duo-web/src/test/java/tdd/duo/web/MvcTestUtil.java
@@ -0,0 +1,16 @@
+package tdd.duo.web;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import tdd.duo.config.WebConfig;
+
+/**
+ * Created by yoon on 15. 4. 6..
+ */
+public class MvcTestUtil {
+
+    public static MockMvc getMockMvc(Object controller) {
+        WebConfig webConfig = new WebConfig();
+        return MockMvcBuilders.standaloneSetup(controller).setViewResolvers(webConfig.internalResourceViewResolver()).build();
+    }
+}
