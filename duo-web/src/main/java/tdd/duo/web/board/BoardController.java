d3c7d5aa46ac7a142b89138dc1b14fae2d793cfd
Yoonsung <lvev9925@naver.com>
2015-04-14 11:39:09 +0900

    Add Article Creation Test

--- duo-web/src/main/java/tdd/duo/web/board/BoardController.java
+++ duo-web/src/main/java/tdd/duo/web/board/BoardController.java
@@ -1,7 +1,12 @@
 package tdd.duo.web.board;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import sun.plugin2.main.client.MacOSXMozillaServiceDelegate;
+import tdd.duo.domain.Article;
 
 /**
  * Created by yoon on 15. 4. 14..
@@ -10,9 +15,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/board")
 public class BoardController {
 
+    private ArticleService articleService;
+
     @RequestMapping("")
     public String list() {
         return "/board/list";
+    }
+
+    @RequestMapping("/register")
+    public String creationForm() {
+        return "/board/register";
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String create(Article article, Model model) {
+
+        try {
+            articleService.create(article);
+        } catch (ArticleCreationException exception) {
+            model.addAttribute(article.getTitle());
+            model.addAttribute(article.getContent());
+            model.addAttribute(exception.getMessage());
+            return "/board/form";
+        }
+
+        return "redirect:/board/list";
     }
 
 }
--- duo-web/src/test/java/tdd/duo/web/board/BoardControllerTest.java
+++ duo-web/src/test/java/tdd/duo/web/board/BoardControllerTest.java
@@ -2,21 +2,23 @@ package tdd.duo.web.board;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import tdd.duo.config.DBConfig;
 import tdd.duo.config.WebConfig;
 import tdd.duo.web.MvcTestUtil;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 /**
  * Created by yoon on 15. 4. 14..
  */
+@RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration(classes = {DBConfig.class})
 public class BoardControllerTest {
 
     private MockMvc mockMvc;
@@ -25,7 +27,6 @@ public class BoardControllerTest {
     public void setUp() {
         this.mockMvc = MvcTestUtil.getMockMvc(new BoardController());
     }
-
 
     @Test
     public void listViewRequest() throws Exception {
@@ -36,5 +37,30 @@ public class BoardControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name(expectedUrl))
                 .andExpect(forwardedUrl(WebConfig.RESOLVER_PREFIX + expectedUrl + WebConfig.RESOLVER_SUFFIX));
+    }
+
+    @Test
+    public void creationViewRequest() throws Exception {
+
+        String expectedUrl = "/board/register";
+
+        mockMvc.perform(get("/board/register"))
+                .andExpect(status().isOk())
+                .andExpect(view().name(expectedUrl))
+                .andExpect(forwardedUrl(WebConfig.RESOLVER_PREFIX + expectedUrl + WebConfig.RESOLVER_SUFFIX));
+    }
+
+    @Test
+    public void createNewArticle() throws Exception {
+        //TODO 첨부파일 업로드
+        String title = "TITLE";
+        String content = "CONTENT";
+
+        mockMvc.perform(post("/board")
+                .param("title", title)
+                .param("content", content)
+        )
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/board/list"));
     }
 }
